772c981b7c0062727996aa6ccc077b66d18da0b6
clarababette <clara_coetzee@zoho.com>
2021-10-26 11:09:03 +0200

    unit test

--- test/waiter-webapp.test.js
+++ test/waiter-webapp.test.js
@@ -19,9 +19,27 @@ describe('The Waiter Scheduling app', () => {
   });
 
   it('should get names and employee ids of all waiters.', async () => {
-    const expected = [];
+    const expected = [
+      {first_name: 'Dave', last_name: 'Avramov', employee_id: 'DavAvr'},
+      {first_name: 'Ilse', last_name: 'King', employee_id: 'IlsKin'},
+      {first_name: 'Benjy', last_name: 'Pafford', employee_id: 'BenPaf'},
+      {first_name: 'Amelie', last_name: 'Loidl', employee_id: 'AmeLoi'},
+      {
+        first_name: 'Karlene',
+        last_name: 'Maciaszek',
+        employee_id: 'KarMac',
+      },
+      {first_name: 'Tiena', last_name: 'Chesney', employee_id: 'TieChe'},
+      {first_name: 'Janaya', last_name: 'Baff', employee_id: 'JanBaf'},
+      {first_name: 'Jean', last_name: 'Guillotin', employee_id: 'JeaGui'},
+      {first_name: 'Daune', last_name: 'Blann', employee_id: 'DauBla'},
+      {
+        first_name: 'Alessandra',
+        last_name: 'Tale',
+        employee_id: 'AleTal',
+      },
+    ];
     const result = await waiters.getAllWaiters();
-    console.log(result);
     assert.deepStrictEqual(result, expected);
   });
 
